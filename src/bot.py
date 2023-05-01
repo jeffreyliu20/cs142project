3a5f6dc603cf75bcc525ce517a7d06c7bc51012f
Ned Tagtmeier <ntagtmeier@uchicago.edu>
2023-04-30 19:27:32 -0500

    Bot works

--- src/bot.py
+++ src/bot.py
@@ -4,10 +4,42 @@ recording the results.
 Currently only functional for the ReversiStub class.
 """
 import sys
+from reversi import ReversiBase
 from mocks import ReversiStub
-from typing import Union
+from typing import Union, Tuple
+import random
 
-NUM_GAMES = sys.argv[1]
+def choose_random_move(revers: ReversiBase) -> Tuple[int, int]:
+    """
+    Chooses a move at random from available moves in a Reversi game
+
+    Parameters:
+      revers[ReversiBase]: a reversi Game
+    
+    Returns[Tuple[int, int]]: coordinates corresponding to a move
+    """
+    return random.choice(revers.available_moves)
+
+def play_game() -> str:
+    """
+    Simulates a game of Reversi between two bots
+    
+    Parameters: none
+    
+    Returns [str]: "Player X wins" where x is the winning player or "Tie"
+    """
+    game = ReversiStub(side=8, players=2, othello=False)
+
+    while not game.done:
+        move = choose_random_move(game)
+        game.apply_move(move)
+
+    if len(game.outcome) > 1:
+        return "Tie"
+    winning_player = str(game.outcome[0])
+    return f"Player {winning_player} wins"
+
+NUM_GAMES = int(sys.argv[1])
 games_played = 0
 results = {"Player 1 wins": 0, "Player 2 wins": 0, "Tie": 0}
 
@@ -16,8 +48,9 @@ while games_played < NUM_GAMES:
     games_played += 1
 
 for key, value in results.items():
-    percentage = value / NUM_GAMES
+    percentage = value / NUM_GAMES * 100
     print(f"{key}: {percentage}%")
+
 
 
 
