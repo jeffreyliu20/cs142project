587cba516e354e37337707488251a95ddbc01451
Jeffrey <jeffreyliu48@gmail.com>
2023-05-10 21:53:30 -0500

    20x20 testing

--- tests/test_reversi.py
+++ tests/test_reversi.py
@@ -305,3 +305,239 @@ def test_winner_1():
     assert reversi.done
     assert reversi.outcome == [2]
 
+
+def test_othello_size_6x6():
+    """
+    Test whether we can correctly create an 8x8 Othello game
+    """
+    reversi = Reversi(side=6, players=2, othello=True)
+
+    assert reversi.size == 6
+
+
+def test_othello_players_6x6():
+    """
+    Test whether we can correctly create a game with 2 players
+    """
+    reversi = Reversi(side=6, players=2, othello=True)
+
+    assert reversi.num_players == 2
+
+
+
+def test_othello_turn_6x6():
+    """
+    Test whether we can correctly create a game on turn 1
+    """
+    reversi = Reversi(side=6, players=2, othello=True)
+
+    assert reversi.turn == 1
+
+
+def test_piece_at_6x6():
+    """
+    Test that piece_at returns correct values
+    in an 6x6 Othello game with no moves made yet
+    """
+    reversi = Reversi(side=6, players=2, othello=True)
+
+    othello_pos = [(2, 2, 2), (2, 3, 1), (3, 2, 1), (3, 3, 2)]
+
+
+    for r, c, expected_piece in othello_pos:
+        piece = reversi.piece_at((r, c))
+        assert (
+            piece == expected_piece
+        ), f"Expected piece_at(({r},{c})) to return {expected_piece} but got {piece}"
+
+
+def test_available_moves_6x6():
+    """
+    Test that available_moves returns correct values
+    in an 6x6 Othello game with no moves made yet
+    """
+    reversi = Reversi(side=6, players=2, othello=True)
+
+    expected = {
+        (1, 2),
+        (2, 1),
+        (4, 3),
+        (3, 4),
+    }
+
+    assert set(reversi.available_moves) == expected
+
+
+def test_legal_move_6x6():
+    """
+    Test that legal_move returns correct values
+    in an 6x6 Othello game with no moves made yet
+    """
+
+    reversi = Reversi(side=6, players=2, othello=True)
+
+    legal = {
+        (1, 2),
+        (2, 1),
+        (4, 3),
+        (3, 4),
+    }
+
+    for r in range(6):
+        for c in range(6):
+            if (r, c) in legal:
+                assert reversi.legal_move(
+                    (r, c)
+                ), f"{(r,c)} is a legal move, but legal_move returned False"
+            else:
+                assert not reversi.legal_move(
+                    (r, c)
+                ), f"{(r,c)} is not a legal move, but legal_move returned True"
+
+
+def test_othello_size_20x20():
+    """
+    Test whether we can correctly create an 20x20 Othello game
+    """
+    reversi = Reversi(side=20, players=2, othello=True)
+
+    assert reversi.size == 20
+
+
+def test_othello_players_20x20():
+    """
+    Test whether we can correctly create a game with 2 players
+    """
+    reversi = Reversi(side=20, players=2, othello=True)
+
+    assert reversi.num_players == 2
+
+
+
+def test_othello_turn_20x20():
+    """
+    Test whether we can correctly create a game on turn 1
+    """
+    reversi = Reversi(side=20, players=2, othello=True)
+
+    assert reversi.turn == 1
+
+
+def test_piece_at_20x20():
+    """
+    Test that piece_at returns correct values
+    in an 6x6 Othello game with no moves made yet
+    """
+    reversi = Reversi(side=20, players=2, othello=True)
+
+    othello_pos = [(9, 9, 2), (9, 10, 1), (10, 9, 1), (10, 10, 2)]
+
+
+    for r, c, expected_piece in othello_pos:
+        piece = reversi.piece_at((r, c))
+        assert (
+            piece == expected_piece
+        ), f"Expected piece_at(({r},{c})) to return {expected_piece} but got {piece}"
+
+
+def test_available_moves_20x20():
+    """
+    Test that available_moves returns correct values
+    in an 20x20 Othello game with no moves made yet
+    """
+    reversi = Reversi(side=20, players=2, othello=True)
+
+    expected = {
+        (8, 9),
+        (9, 8),
+        (11, 10),
+        (10, 11),
+    }
+
+    assert set(reversi.available_moves) == expected
+
+
+def test_legal_move_20x20():
+    """
+    Test that legal_move returns correct values
+    in an 20x20 Othello game with no moves made yet
+    """
+
+    reversi = Reversi(side=20, players=2, othello=True)
+
+    legal = {
+        (8, 9),
+        (9, 8),
+        (11, 10),
+        (10, 11),
+    }
+
+    for r in range(6):
+        for c in range(6):
+            if (r, c) in legal:
+                assert reversi.legal_move(
+                    (r, c)
+                ), f"{(r,c)} is a legal move, but legal_move returned False"
+            else:
+                assert not reversi.legal_move(
+                    (r, c)
+                ), f"{(r,c)} is not a legal move, but legal_move returned True"
+
+    """
+    Test that available_moves returns correct values
+    in an 20x20 Othello game with no moves made yet
+    """
+    reversi = Reversi(side=20, players=2, othello=True)
+
+    expected = {
+        (8, 9),
+        (9, 8),
+        (11, 10),
+        (10, 11),
+    }
+
+    assert set(reversi.available_moves) == expected
+
+
+def test_available_moves_8x8_non_othello():
+    """
+    Test that available_moves returns correct values
+    in an 8x8 Reversi game with no moves made yet
+    """
+    reversi = Reversi(side=8, players=2, othello=False)
+
+    assert reversi.first_two
+    expected = {
+        (3, 3),
+        (3, 4),
+        (4, 3),
+        (4, 4)
+    }
+
+    assert set(reversi.available_moves) == expected
+
+def test_legal_move_8x8_non_othello():
+    """
+    Test that legal_move returns correct values
+    in an 8x8 Reversi game with no moves made yet
+    """
+
+    reversi = Reversi(side=8, players=2, othello=False)
+
+    legal = {
+        (3, 3),
+        (3, 4),
+        (4, 3),
+        (4, 4)
+    }
+
+    for r in range(6):
+        for c in range(6):
+            if (r, c) in legal:
+                assert reversi.legal_move(
+                    (r, c)
+                ), f"{(r,c)} is a legal move, but legal_move returned False"
+            else:
+                assert not reversi.legal_move(
+                    (r, c)
+                ), f"{(r,c)} is not a legal move, but legal_move returned True"
