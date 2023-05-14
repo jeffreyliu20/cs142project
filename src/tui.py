d41644fd74768d7a399f644bd968f933f8ce1ad2
ewoldu <116973160+ewoldu@users.noreply.github.com>
2023-05-14 13:44:52 -0500

    Added click interface

--- src/tui.py
+++ src/tui.py
@@ -2,11 +2,9 @@
 import sys
 from typing import List, Optional, Tuple, Set, Callable
 from mocks import ReversiStub, ReversiMock
-from reversi import BoardGridType
+from reversi import BoardGridType, Reversi
 
-board_size: int = int(sys.argv[1])
-num_players: int = 2
-othello: bool = True
+import click
 
 WALL_CHARS = {
     "H_WALL": "─", "V_WALL": "│", "HV_WALL": "┼",
@@ -34,94 +32,54 @@ CLOCK_CHARS = {
     (True, True, True, True): WALL_CHARS["HV_WALL"]
 }
 
-game = ReversiMock(board_size, num_players, othello)
-
-if 6 <= board_size <= 20:
-
-    grid_size = 2 * board_size + 1
+@click.command()
+@click.option('-n', default=2, show_default=True,
+              help="Number of Players in the game")
+@click.option('-s', default=8, show_default=True,
+              help="Length of the sides of the board")
+@click.option('--othello/--non-othello', default=True, show_default=True,
+              help="Whether or not the game is othello or not othello")
+def play_game(n, s, othello):
     
-    board = []
+    num_players = n
+    board_size = s
 
-    for i in range(grid_size):
-        board_row = []
-        for j in range(grid_size):
-            board_row.append("")
-        board.append(board_row)
+    game = Reversi(board_size, num_players, othello)
 
-    for r in range(grid_size):
-        for c in range(grid_size):
-            grid_dir = [True] * 4
-            
-            if r % 2 != 0:
-                grid_dir[1] = False
-                grid_dir[3] = False
-            elif r == 0:
-                grid_dir[0] = False
-            elif r == len(board) - 1:
-                grid_dir[2] = False
-            
-            if c % 2 != 0:
-                grid_dir[0] = False
-                grid_dir[2] = False
-            elif c == 0:
-                grid_dir[3] = False
-            elif c == len(board) - 1:
-                grid_dir[1] = False
+    if 6 <= board_size <= 20:
 
-            tup = (grid_dir[0], grid_dir[1], grid_dir[2], grid_dir[3])
-            board[r][c] = CLOCK_CHARS[tup]
-
-    for x, row in enumerate(game.grid):
-        for y, cell in enumerate(row):
-            if game.grid[x][y] is not None:
-                board[2 * x + 1][2 * y + 1] = f"{cell}"
-
-    board_row = []
-    for row2 in board:
-        board_row.append("".join(row2))
-
-    board_str = "\n".join(board_row)
-    print(board_str)
-
-    earlyEnd = False
-
-    while not game.done:
-
-        available_moves = game.available_moves
-
-        print("Choose one of the following move options")
-        print()
-
-        for k, move in enumerate(available_moves):
-            i, j = move
-            print(f"{k+1}) {i}, {j}")
+        grid_size = 2 * board_size + 1
         
-        print()
-        print("If you want to exit the game, type 'quit' and then press Enter")
-        choice = input("Enter your choice and then press Enter: ")
-        print()
-        
-        while True:
-            if choice == "quit":
-                break
-            if choice.isdigit() and 0 < int(choice) <= len(available_moves):
-                break
-            print("You must input an integer that is the same as one of the " +
-                  "options")
-            choice = input("Enter your choice and then press Enter: ")
-            print()
+        board = []
 
-        if choice == "quit":
-            earlyEnd = True
-            break
+        for i in range(grid_size):
+            board_row = []
+            for j in range(grid_size):
+                board_row.append("")
+            board.append(board_row)
 
-        move_r, move_c = available_moves[int(choice)-1]
+        for r in range(grid_size):
+            for c in range(grid_size):
+                grid_dir = [True] * 4
+                
+                if r % 2 != 0:
+                    grid_dir[1] = False
+                    grid_dir[3] = False
+                elif r == 0:
+                    grid_dir[0] = False
+                elif r == len(board) - 1:
+                    grid_dir[2] = False
+                
+                if c % 2 != 0:
+                    grid_dir[0] = False
+                    grid_dir[2] = False
+                elif c == 0:
+                    grid_dir[3] = False
+                elif c == len(board) - 1:
+                    grid_dir[1] = False
 
-        if not game.legal_move((move_r, move_c)):
-            print("Not a legal move, please try again")
-            continue
-
-        game.apply_move((move_r, move_c))
+                tup = (grid_dir[0], grid_dir[1], grid_dir[2], grid_dir[3])
+                board[r][c] = CLOCK_CHARS[tup]
 
         for x, row in enumerate(game.grid):
             for y, cell in enumerate(row):
@@ -135,13 +93,68 @@ if 6 <= board_size <= 20:
         board_str = "\n".join(board_row)
         print(board_str)
 
-    if earlyEnd:
-        print("Game Ended early")
+        earlyEnd = False
+
+        while not game.done:
+
+            available_moves = game.available_moves
+
+            print("Choose one of the following move options")
+            print()
+
+            for k, move in enumerate(available_moves):
+                print(move)
+                i, j = move
+                print(f"{k+1}) {i}, {j}")
+            
+            print()
+            print("If you want to exit the game, type 'quit' and then press Enter")
+            choice = input("Enter your choice and then press Enter: ")
+            print()
+            
+            while True:
+                if choice == "quit":
+                    break
+                if choice.isdigit() and 0 < int(choice) <= len(available_moves):
+                    break
+                print("You must input an integer that is the same as one of the " +
+                    "options")
+                choice = input("Enter your choice and then press Enter: ")
+                print()
+
+            if choice == "quit":
+                earlyEnd = True
+                break
+
+            move_r, move_c = available_moves[int(choice)-1]
+
+            if not game.legal_move((move_r, move_c)):
+                print("Not a legal move, please try again")
+                continue
+
+            game.apply_move((move_r, move_c))
+
+            for x, row in enumerate(game.grid):
+                for y, cell in enumerate(row):
+                    if game.grid[x][y] is not None:
+                        board[2 * x + 1][2 * y + 1] = f"{cell}"
+
+            board_row = []
+            for row2 in board:
+                board_row.append("".join(row2))
+
+            board_str = "\n".join(board_row)
+            print(board_str)
+
+        if earlyEnd:
+            print("Game Ended early")
+        else:
+            winners = game.outcome
+            for person in winners:
+                print(f"Congrats for Player {person} for a nice victory")
+
+
     else:
-        winners = game.outcome
-        for person in winners:
-            print(f"Congrats for Player {person} for a nice victory")
+        print("Invalid board size, please try again")
 
-
-else:
-    print("Invalid board size, please try again")
+play_game()
