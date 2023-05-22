649f8c22527a1c28e1dbdbe9f0a6468fd991a2bb
Sylvia Kim <sylviakim@uchicago.edu>
2023-05-22 15:13:03 -0500

    gui update

--- src/gui.py
+++ src/gui.py
@@ -5,19 +5,26 @@ from reversi import Reversi
 from math import sqrt
 
 import pygame
+import click
 
 pygame.init()
-    
-reversi = Reversi(int(sys.argv[1]), 2, True)
+
 white = (255, 255, 255)
 green = (0, 255, 0)
 blue = (0, 0, 128)
 
-class Board:
+@click.command()
+@click.option('-n', '--num_players', default=2, show_default=True, type=int,
+              help="Number of Players in the game")
+@click.option('-s', '--board_size', default=8, show_default=True, type=int,
+              help="Length of the sides of the board")
+@click.option('--othello/--non-othello', default=False, show_default=True,
+              help="Whether or not the game is othello or not othello")
+
+class ReversiGUI:
     """
     Class for a GUI-based bitmap editor
     """
-
     window : int
     border : int
     grid : List[List[bool]]
@@ -25,7 +32,7 @@ class Board:
     clock : pygame.time.Clock
     reversi : Reversi
 
-    def __init__(self, reversi: Reversi, window: int = 600, border: int = 10,
+    def __init__(self, num_players : int, board_size: int, othello : bool, window: int = 600, border: int = 10,
                  cells_side: int = 32):
         """
         Constructor
@@ -35,14 +42,14 @@ class Board:
             border : int : number of pixels to use as border around elements
             cells_side : int : number of cells on a side of a square bitmap grid
         """
-        self.reversi = reversi
-        if len(reversi.grid) > 20 or len(reversi.grid) < 6:
+        if board_size > 20 or board_size < 6:
             print("The board size is not valid")
             sys.exit()
         
         self.window = window
         self.border = border
         self.grid = [[False] * cells_side for i in range(cells_side)]
+        self.reversi = Reversi(board_size, num_players, othello)
 
         pygame.init()
         pygame.display.set_caption("BitEdit")
@@ -65,10 +72,6 @@ class Board:
         self.surface.fill((128, 128, 128))
 
         square = (self.window - 2 * self.border) // cells_side
-        # mini_left = 2 * self.border + square * cells_side
-        # mini_top = (self.window - cells_side) // 2
-
-        # rect = (mini_left, mini_top, cells_side, cells_side)
 
         for row in range(len(self.reversi.grid)):
             for col in range(len(self.reversi.grid[row])):
@@ -81,14 +84,16 @@ class Board:
                 pygame.draw.rect(self.surface, color=(0, 0, 0),
                                      rect=rect, width=1)
 
+        player_colors = [(255, 0, 0), (0, 0, 0), (0, 255, 0), (0, 0, 255), (255, 215, 0), (191,62,255), (0, 238, 238), (255, 52, 179), (205, 186, 150)]
         for row in range(len(self.reversi.grid)):
             for col in range(len(self.reversi.grid[row])):   
                 if self.reversi.grid[row][col]:
-                    color = (255, 0, 0) if self.reversi.grid[row][col] == 1 else (0, 0, 0)
-                    pygame.draw.circle(self.surface, color=color,
+                    color = self.reversi.grid[row][col] - 1
+                    pygame.draw.circle(self.surface, color=player_colors[color],
                            center=(self.border + col * square + square / 2, self.border + row * square + square /2), radius=cells_side * 3,
                            width=10)
         
+
         pygame.display.set_caption('Show Text')
         font = pygame.font.Font('freesansbold.ttf', 20)
         text = font.render(f"Player {self.reversi.turn}", True, white, blue)
@@ -148,4 +153,4 @@ class Board:
 
 
 if __name__ == "__main__":
-    Board(reversi)
+    ReversiGUI()
