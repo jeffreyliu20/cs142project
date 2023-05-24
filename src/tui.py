4d04b1e0b80f51e031b9c194e95129b38053fe0f
ewoldu <116973160+ewoldu@users.noreply.github.com>
2023-05-24 13:49:18 -0500

    Made method to clean up code and fix pylint issues

--- src/tui.py
+++ src/tui.py
@@ -4,8 +4,7 @@ the player if their turn is skipped, which I can't do using that method
 """
 
 
-import sys
-from typing import List, Optional, Tuple, Set, Callable
+from typing import List, Tuple
 from reversi import BoardGridType, Reversi, ListMovesType
 from bot import choose_random_move, choose_high_n_move, choose_high_m_move
 
@@ -49,87 +48,39 @@ CLOCK_CHARS = {
               help="What type of bot strategy you want to use")
 
 def play_game(num_players: int, board_size: int, othello: bool, bot: str):
-    
+    """
+    Runs the reversi game in a textual user interface after taking in some
+    command line arguments.
+    """
+
     if board_size < 3:
         print("Board size must be 3 or greater. Please try again.")
-    elif not (2 <= num_players <= 9):
+    elif not 2 <= num_players <= 9:
         print("Number of players must be between 2 & 9")
     elif num_players % 2 != board_size % 2:
-        print("Board size and number of players must both be odd or both be " + 
+        print("Board size and number of players must both be odd or both be " +
               "even. Please try again")
     else:
-
         game: Reversi = Reversi(board_size, num_players, othello)
 
         bot_in_game: bool = bot != "none"
-        
+
         grid_size: int = 2 * board_size + 1
-        
         board: List[List[str]] = []
 
-        for i in range(grid_size):
-            board_row: List[str] = []
-            for j in range(grid_size):
-                board_row.append("")
-            board.append(board_row)
-
-        for r in range(grid_size):
-            for c in range(grid_size):
-                grid_dir = [True] * 4
-                
-                if r % 2 != 0:
-                    grid_dir[1] = False
-                    grid_dir[3] = False
-                elif r == 0:
-                    grid_dir[0] = False
-                elif r == len(board) - 1:
-                    grid_dir[2] = False
-                
-                if c % 2 != 0:
-                    grid_dir[0] = False
-                    grid_dir[2] = False
-                elif c == 0:
-                    grid_dir[3] = False
-                elif c == len(board) - 1:
-                    grid_dir[1] = False
-
-                tup = (grid_dir[0], grid_dir[1], grid_dir[2], grid_dir[3])
-                board[r][c] = CLOCK_CHARS[tup]
-
-        for x, row in enumerate(game.grid):
-            for y, cell in enumerate(row):
-                if game.grid[x][y] != 0:
-                    board[2 * x + 1][2 * y + 1] = f"{cell}"
-
-        board_row = [" "]
-        bottom_row = ""
-        for a in range(grid_size):
-            if a % 2 == 0:
-                bottom_row += " "
-            else:
-                bottom_row += f"{a // 2}"
-
-        for b, row2 in enumerate(board):
-            if b % 2 == 1:
-                board_row.append("".join(row2) + f"{b // 2}")
-            else:
-                board_row.append("".join(row2))
-
-        board_row.append(bottom_row)
-        board_str = "\n".join(board_row)
+        board_str, board = make_board_str(grid_size, game.grid, board, True)
         print(board_str)
 
         if bot_in_game:
             print()
             print(f"The bot will be Player {num_players}")
 
-        earlyEnd: bool = False
         players_skipped: int = 0
 
         while not game.done and players_skipped < num_players:
 
             moves: ListMovesType = game.available_moves
-            turn: int = game._turn
+            turn: int = game.turn
 
             if len(moves) == 0:
                 print()
@@ -166,13 +117,13 @@ def play_game(num_players: int, board_size: int, othello: bool, bot: str):
                     for k, move in enumerate(moves):
                         i, j = move
                         print(f"{k+1}) ({i}, {j})")
-                    
+
                     print()
-                    print("If you want to exit the game, type 'quit', " + 
+                    print("If you want to exit the game, type 'quit', " +
                         "then press Enter")
                     choice = input("Enter your choice, then press Enter: ")
                     print()
-                    
+
                     while True:
                         if choice == "quit":
                             break
@@ -183,7 +134,6 @@ def play_game(num_players: int, board_size: int, othello: bool, bot: str):
                         choice = input("Enter your choice, then press Enter: ")
 
                     if choice == "quit":
-                        earlyEnd = True
                         break
 
                     move_r, move_c = moves[int(choice)-1]
@@ -194,20 +144,8 @@ def play_game(num_players: int, board_size: int, othello: bool, bot: str):
 
                 game.apply_move((move_r, move_c))
 
-                for x, row in enumerate(game.grid):
-                    for y, cell in enumerate(row):
-                        if game.grid[x][y] != 0:
-                            board[2 * x + 1][2 * y + 1] = f"{cell}"
-
-                board_row = [" "]
-                for b, row2 in enumerate(board):
-                    if b % 2 == 1:
-                        board_row.append("".join(row2) + f"{b // 2}")
-                    else:
-                        board_row.append("".join(row2))
-
-                board_row.append(bottom_row)
-                board_str = "\n".join(board_row)
+                board_str, board = make_board_str(grid_size, game.grid, board,
+                                                  False)
                 print(board_str)
 
         game.end_game()
@@ -218,6 +156,81 @@ def play_game(num_players: int, board_size: int, othello: bool, bot: str):
             print("We have a tie between the following players:")
             for person in winners:
                 print(f"Player {person}")
+
+
+def make_board_str(grid_size: int, grid: BoardGridType, board: List[List[str]],
+                   first_time) -> Tuple[str, List[List[str]]]:
+    """
+    Creates a string object representing the board that will be printed given
+    a certain 2x2 matrix of strings. If it is the first time this method is
+    called, it builds the board as well. Otherwise, it updates the board as the
+    moves are made
+
+    Inputs:
+        grid_size: side lengths of the string representation
+        grid: the reversi game grid that has where the board pieces are
+        board: the string matrix that is used to create the string representation
+        first_time: a boolean indicating whether or not this is the first_time
+        this method has been called
+
+    Returns:
+        Returns a tuple containing the string representation and the matrix
+        used to create that string representation
+    """
+
+    if first_time:
+        for _ in range(grid_size):
+            board_row: List[str] = []
+            for _ in range(grid_size):
+                board_row.append("")
+            board.append(board_row)
+
+        for r in range(grid_size):
+            for c in range(grid_size):
+                grid_dir = [True] * 4
+
+                if r % 2 != 0:
+                    grid_dir[1] = False
+                    grid_dir[3] = False
+                elif r == 0:
+                    grid_dir[0] = False
+                elif r == len(board) - 1:
+                    grid_dir[2] = False
+
+                if c % 2 != 0:
+                    grid_dir[0] = False
+                    grid_dir[2] = False
+                elif c == 0:
+                    grid_dir[3] = False
+                elif c == len(board) - 1:
+                    grid_dir[1] = False
+
+                tup = (grid_dir[0], grid_dir[1], grid_dir[2], grid_dir[3])
+                board[r][c] = CLOCK_CHARS[tup]
+
+    for x, row in enumerate(grid):
+        for y, cell in enumerate(row):
+            if grid[x][y] != 0:
+                board[2 * x + 1][2 * y + 1] = f"{cell}"
+
+    board_row = [" "]
+    bottom_row = ""
+    for i in range(grid_size):
+        if i % 2 == 0:
+            bottom_row += " "
+        else:
+            bottom_row += f"{i // 2}"
+
+    for j, row2 in enumerate(board):
+        if j % 2 == 1:
+            board_row.append("".join(row2) + f"{j // 2}")
+        else:
+            board_row.append("".join(row2))
+
+    board_row.append(bottom_row)
+    board_str = "\n".join(board_row)
+    return board_str, board
+
 
 if __name__ == "__main__":
     play_game()
